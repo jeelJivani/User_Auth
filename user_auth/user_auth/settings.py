3d195aed960961ad5c7d2775af9b86a5c4cf2e58
jeelJivani <123637044+jeelJivani@users.noreply.github.com>
2023-12-08 12:53:55 +0530

    Update settings.py

--- user_auth/user_auth/settings.py
+++ user_auth/user_auth/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_app',
-    'bootstrap4',
+    'user_app'
 ]
 
 MIDDLEWARE = [
